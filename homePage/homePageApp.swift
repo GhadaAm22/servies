91c32ee3c0d87ef218de0eb6f1347aa22cd3b1d4
ghada <ghada_alshahrani@hotmail.com>
2022-10-26 19:14:01 +0300

    Initial Commit

---
+++ homePage/homePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  homePageApp.swift
+//  homePage
+//
+//  Created by Ghada Amer Alshahrani on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct homePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
